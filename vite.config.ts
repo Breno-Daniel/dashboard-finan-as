84ea4f4ea1cc6c0cfa8fba7126e9b95113462f17
Breno-Daniel <109623004+Breno-Daniel@users.noreply.github.com>
2025-06-22 16:27:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dashboard-finan-as/', // coloque exatamente o nome do seu repositório
+  base: '/dashboard-finan-as/',
 })
